383839b6efef09053316fbe8542c79a38a63aabd
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-06-21 11:25:50 +0900

    [Added] template (from "mix new"

---
+++ lib/toggxir.ex
@@ -0,0 +1,18 @@
+defmodule Toggxir do
+  @moduledoc """
+  Documentation for `Toggxir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Toggxir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/toggxir_test.exs
@@ -0,0 +1,8 @@
+defmodule ToggxirTest do
+  use ExUnit.Case
+  doctest Toggxir
+
+  test "greets the world" do
+    assert Toggxir.hello() == :world
+  end
+end
