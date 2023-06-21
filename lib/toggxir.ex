8d31d4586835dce55617900e99a66ce7c237c387
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-06-21 12:01:31 +0900

    [Added] function (test)

--- lib/toggxir.ex
+++ lib/toggxir.ex
@@ -15,4 +15,25 @@ defmodule Toggxir do
   def hello do
     :world
   end
+
+  defmodule Toggxir.User do
+    defstruct email: nil, password: nil, token: nil
+
+    def setEmail do
+    end
+
+    def setPassword do
+    end
+
+    def setToken do
+    end
+    
+    
+    def getToken do
+      if (email == nil || password == nil) && token == nil  do
+	
+      end
+    end
+  end
+  
 end
